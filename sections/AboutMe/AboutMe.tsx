5b31450d2505475884cc2c32c61305afb51da01c
Luca <lucagcuello@gmail.com>
2024-07-03 10:47:02 -0300

    Modified Card conditional styles and componentized sections

---
+++ sections/AboutMe/AboutMe.tsx
@@ -0,0 +1,17 @@
+import Card from "@/components/Card/Card";
+
+export default function AboutMe() {
+  return (
+    <Card type="primary">
+      <h1>About me</h1>
+      <p>
+        Hello, I'm John Williams, a composer, conductor, and pianist with a
+        career spanning over six decades. I've had the privilege of creating
+        music for some of the most beloved films in cinematic history, including
+        "Star Wars," "Jaws," "E.T. the Extra-Terrestrial," "Indiana Jones," and
+        "Harry Potter." My passion lies in crafting scores that elevate
+        storytelling, bringing characters and worlds to life through music.
+      </p>
+    </Card>
+  );
+}
