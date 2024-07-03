9334a843e4c7f5cfdfc74ae1be6f0015b511476a
Luca <lucagcuello@gmail.com>
2024-07-03 20:14:04 -0300

    Form functionality, validation and dependecies installation

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,15 +1,67 @@
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Toaster, toast } from "sonner";
 import Button from "../Button/Button";
 import styles from "./form.module.css";
 
+interface IFormInput {
+  fullName: string;
+  email: string;
+  message: string;
+}
+
 export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    toast.success("Message sent!");
+    reset();
+  };
+
+  const onError = () => {
+    toast.error("Please fill in all fields!");
+  };
+
   return (
-    <form className={styles.form}>
-      <div>
-        <input type="text" placeholder="Your full name" />
-        <input type="email" placeholder="Your email" />
-      </div>
-      <textarea placeholder="I wanna talk about..." />
-      <Button>Let's talk!</Button>
-    </form>
+    <div>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit, onError)}>
+        <div>
+          <div className={styles.inputContainer}>
+            <input
+              type="text"
+              placeholder="Your full name"
+              {...register("fullName", { required: true })}
+            />
+            {errors.fullName && (
+              <p className={styles.error}>Full name can't be empty.</p>
+            )}
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              type="email"
+              placeholder="Your email"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className={styles.error}>Email can't be empty.</p>
+            )}
+          </div>
+        </div>
+        <textarea
+          placeholder="I wanna talk about..."
+          {...register("message", { required: true })}
+        />
+        {errors.message && (
+          <p className={styles.error}>You need to write something</p>
+        )}
+        <Button>Let's talk!</Button>
+      </form>
+      <Toaster position="top-right" />
+    </div>
   );
 }
